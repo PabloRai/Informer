d2f06dd2a7ebf68015396d335a180d72409667ef
PabloRai <pabloaraimondo@gmail.com>
2018-01-22 19:54:50 -0300

    Method refactor

--- src/Informer.groovy
+++ src/Informer.groovy
@@ -4,14 +4,20 @@ import org.jsoup.nodes.Document
 
 class Main {
     static void main(String[] args) {
-        Document document = Jsoup.connect("http://www.ravaonline.com/v2/precios/panel.php?m=GEN").get()
+        Set<Ticker> tickers = getTickers("http://www.ravaonline.com/v2/precios/panel.php?m=GEN")
+        tickers.addAll(getTickers("http://www.ravaonline.com/v2/precios/panel.php?m=LID"))
+        println(tickers)
+    }
+
+    private static List<Ticker> getTickers(String uri) {
+        Document document = Jsoup.connect(uri).get()
         Element table = document.select("table").get(4)
         ArrayList nodes = table.select("tr").childNodes
-        nodes = nodes.subList(9, nodes.size()-1)
-        ArrayList<Ticker> tickers = nodes.collect {
-                new Ticker (it)
-            }
-        
+        nodes = nodes.subList(9, nodes.size() - 1)
+        return nodes.collect {
+            new Ticker(it)
+        }
+
     }
 
 }
--- src/Ticker.groovy
+++ src/Ticker.groovy
@@ -1,26 +1,34 @@
 class Ticker {
     String tickerId
-    String lastValue
-    String dayPerent
-    String previousValue
-    String openValue
-    String minimumValue
-    String maxValue
-    String nominalVolume
-    String effectiveVolume
+    double lastValue
+    double dayPerent
+    double previousValue
+    double openValue
+    double minimumValue
+    double maxValue
+    double nominalVolume
+    double effectiveVolume
 
     static  String[] props = Ticker.declaredFields.findAll {
         (!it.toString().contains("static") && !it.toString().contains("transient") ) ? it.toString() : null
-    }.collect {it.toString().substring(32)}
+    }.collect {it.toString().substring(it.toString().lastIndexOf('.')+1)}
     Ticker( data) {
         ArrayList<String> cleanData = data.collect {
            if(it.text().trim() != "" && !it.text().trim().contains(":")) {
-               return it.text().replaceAll(",", ".")
+               return it.text()
            }
         }
         cleanData.removeAll([null])
+        for (int i = 0; i < cleanData.size() ; i++) {
+            cleanData[i] = cleanData[i].replace(".", "")
+            cleanData[i] = cleanData[i].replace(",", ".")
+        }
         for (int i = 0; i < cleanData.size(); i++) {
-            this."${props[i]}" = cleanData[i]
+            if(!cleanData[i].isNumber()) {
+                this."${props[i]}" = cleanData[i]
+            } else {
+                this."${props[i]}" = cleanData[i] as double
+            }
         }
 
     }
