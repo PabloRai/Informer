a06ec74d07265baebae803707c2a8e815ad45201
PabloRai <pabloaraimondo@gmail.com>
2018-01-23 23:17:01 -0300

    Get historical data of all the tickers

--- src/Informer.groovy
+++ src/Informer.groovy
@@ -1,21 +1,35 @@
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
 import org.jsoup.nodes.Document
+import static groovyx.gpars.GParsPool.withPool
 
 class Main {
     static void main(String[] args) {
-        Set<Ticker> tickers = getTickers("http://www.ravaonline.com/v2/precios/panel.php?m=GEN")
+        Set<String> tickers = getTickers("http://www.ravaonline.com/v2/precios/panel.php?m=GEN")
         tickers.addAll(getTickers("http://www.ravaonline.com/v2/precios/panel.php?m=LID"))
-        println(tickers)
+        tickers = tickers.sort()
+        InputStream input = null
+        Set<Reader> readers = new HashSet<>(75)
+        Map<String, Reader> mReader = new HashMap<>(75)
+        withPool(32) {
+            tickers.collectParallel {
+                input = new URL("http://www.ravaonline.com/v2/empresas/precioshistoricos.php?e=${it}&csv=1").openStream()
+                mReader.put(it, new InputStreamReader(input, "UTF-8"))
+            }
+        }
+        println(mReader)
+
+
+
     }
 
-    private static List<Ticker> getTickers(String uri) {
+    private static HashSet<String> getTickers(String uri) {
         Document document = Jsoup.connect(uri).get()
         Element table = document.select("table").get(4)
         ArrayList nodes = table.select("tr").childNodes
         nodes = nodes.subList(9, nodes.size() - 1)
         return nodes.collect {
-            new Ticker(it)
+            it[1].text()
         }
 
     }
